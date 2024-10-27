719ee9b731ec48d980e201c7f444b5782f8c2fbd
Eliza Malyshev <142819935+Eli-Xen@users.noreply.github.com>
2024-10-26 19:41:23 -0500

    Add files via upload

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+printf("\n"); 
+    // for (int i=0; i<SIZE; i++) //goes through each row of matrix 2... 
+    // {
+    //     printf("give row %d of matrix 2, with each integer seperated by spaces: ", i+1); 
+    //     for (int j=0; j<SIZE; j++) //...and for every column...
+    //     {
+    //         scanf("%d", &m2[i][j]); //...read each int seperatley becuase white space seperates them 
+    //     }
+
+    // }
